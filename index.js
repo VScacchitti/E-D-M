9deab5cf2897493283171da781cab671380a3218
Vince Scacchitti <VScacchitti24@gmail.com>
2020-10-20 09:39:08 -0400

    created mysql connection in app.js, and added crated db.

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const util = require("util");
 const conTable = require("console.table");
+const { connect } = require("http2");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Wateryzoo098!",
+    database: "employee_db"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    connection.end();
+  });
