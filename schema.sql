8602a6c57d0d0b8271dc4f4c0afff149bab8ce90
Vince Scacchitti <VScacchitti24@gmail.com>
2020-10-27 10:12:43 -0400

    updated the schema with foreign keys to be accessed when joining tables. Updated the seeds to fit this new schema.

--- schema.sql
+++ schema.sql
@@ -6,20 +6,44 @@ USE employee_db;
 
 CREATE TABLE department (
 id INTEGER PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(30) NOT NULL
+deptName VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE roles (
-id INTEGER PRIMARY KEY AUTO_INCREMENT,
-title VARCHAR(30) NOT NULL,
-salary DECIMAL(7,2) NOT NULL,
-department_id INTEGER NOT NULL
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(255),
+salary DECIMAL(10,2),
+department_id INT,
+--Added FOREIGN KEY TO BE REFERENCED LATER
+FOREIGN KEY (department_id)
+REFERENCES department (id)
+-- DELETES all tables referenced by the dept id
+ON
+DELETE CASCADE
+ON
+UPDATE NO ACTION
 );
 
 CREATE TABLE employee (
-id INTEGER PRIMARY KEY AUTO_INCREMENT,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INTEGER NOT NULL,
-manager_id INTEGER
-)
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+role_id INT,
+--to be referenced later
+FOREIGN KEY (role_id)
+REFERENCES role (id)
+---- DELETES all tables referenced 
+ON
+DELETE CASCADE
+ON 
+UPDATE NO ACTION,
+
+manager_id INT,
+FOREIGN KEY (manager_id)
+REFERENCES employee (id)
+---- DELETES all tables referenced 
+ON 
+DELETE CASCADE
+ON
+UPDATE NO ACTION
+);
--- seed.sql
+++ seed.sql
@@ -1,9 +1,54 @@
-INSERT INTO department (name)
-Values ("Human Resources"), ("IT"),("Legal"),("Sales");
+    INSERT INTO department
+    (id, deptName)
+VALUES
+    (1, "Human Resources");
+
+INSERT INTO department
+    (id, deptName)
+VALUES
+    (2, "IT");
+INSERT INTO department
+    (id, deptName)
+VALUES
+    (3, "Legal");
 
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Manager-Sales", 100000, 20), ("Developer", 70000, 40), ("Manager-Project", 120000, 50), ("Attorney", 90000, 60), ("HR Administrator", 55000, 80);
+INSERT INTO role
+    (title, salary, department_id)
+VALUES("HR Administrator", 76296.33, 1);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Aaron", "Smith", 1, NULL), ("Amanda", "Solden", 2, 100), ("Shawna", "Ray", 1, NULL), ("Richard", "Lopez", 1, 101), ("Sandeep", "Aleki", 2, NULL), ("Ashley", "Simon", 4, NULL)
+INSERT INTO role
+    (title, salary, department_id)
+VALUES("Developer", 87526.02, 2);
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES("Attorney", 91,432.47, 3);
+
+
+
+INSERT INTO employee
+    (first_name, last_name, role_id)
+VALUES("Anthony", "Smith", 1);
+
+INSERT INTO employee
+    (first_name, last_name, role_id)
+VALUES("Jon", "Jones", 2);
+
+INSERT INTO employee
+    (first_name, last_name, role_id)
+VALUES("Stipe", "Mioic", 3);
+
+
+
+INSERT INTO employee
+    (first_name, last_name, role_id,manager_id)
+VALUES("Dana", "White", 1, 1);
+
+INSERT INTO employee
+    (first_name, last_name, role_id,manager_id)
+VALUES("Sean", "Shelby", 2, 2);
+
+INSERT INTO employee
+    (first_name, last_name, role_id,manager_id)
+VALUES("Chuck", "Liddel", 3, 3);
